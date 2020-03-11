0e565d4a90818b73ec25e2ac3e9219aa47e31ca3
Manuel Bugallo <marvinody@gmail.com>
2020-03-11 00:30:09 -0400

    changes linting stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-export function doOtherThing() {}
+export function doOtherThing() {
+  console.log('IM DOING ANOTHER THING');
+}
 
 export function doThing() {
-  console.log("IM DOING A THING")
+  console.log('IM DOING A THING');
 }
